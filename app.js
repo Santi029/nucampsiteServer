cc560712ef40d7e4fe554c75d054e0d73a8abecc
Hector L. Santiago <hlsp@me.com>
2021-03-19 23:20:34 -0500

    Passport

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+
 const session = require("express-session");
 const FileStore = require("session-file-store")(session);
+const passport = require("passport");
+const authenticate = require("./authenticate");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
@@ -32,6 +36,9 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -48,39 +55,14 @@ app.use(
 );
 
 function auth(req, res, next) {
-    console.log(req.session);
+    console.log(req.user);
 
-    if (!req.session.user) {
-        const authHeader = req.headers.authorization;
-        if (!authHeader) {
-            const err = new Error("You are not authenticated!");
-            res.setHeader("WWW-Authenticate", "Basic");
-            err.status = 401;
-            return next(err);
-        }
-
-        const auth = Buffer.from(authHeader.split(" ")[1], "base64")
-            .toString()
-            .split(":");
-        const user = auth[0];
-        const pass = auth[1];
-        if (user === "admin" && pass === "password") {
-            req.session.user = "admin";
-            return next(); // authorized
-        } else {
-            const err = new Error("You are not authenticated!");
-            res.setHeader("WWW-Authenticate", "Basic");
-            err.status = 401;
-            return next(err);
-        }
+    if (!req.user) {
+        const err = new Error("You are not authenticated!");
+        err.status = 401;
+        return next(err);
     } else {
-        if (req.session.user === "admin") {
-            return next();
-        } else {
-            const err = new Error("You are not authenticated!");
-            err.status = 401;
-            return next(err);
-        }
+        return next();
     }
 }
 
